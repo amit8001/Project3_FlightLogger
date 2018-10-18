05af5f633e821f401a8b52d491581ba9643b8722
Houssam Moustafa <houssam2@yahoo.com>
2018-10-18 18:21:14 -0400

    minor about change

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -2,21 +2,24 @@ import React from "react";
 import "./About.css";
 
 const About = () => (
-  <div id="about_id"> 
-    <h1>About Page</h1>
+  <div id="about_id">
+    <h1> About </h1>
     <p>
-
-
-
-Sky Logger allows a student pilot or a professional pilot to log their flight time. 
-It is intended to replace or supplement the paper logbook that all pilots have
-historically used. Sky logger looks like the logbook that all pilots are 
-familiar with. In addition, Sky Logger provides aggregate totals, which 
-simplify filling out FAA and insurance applications, which could take a pilot
-significant time to compile.
-
+      Sky Logger allows a student pilot or a professional pilot to log their sky flight time.
     </p>
-  </div>
-);
+    <p>
+      It is intended to replace or supplement the paper logbook that all pilots have
+      historically used.
+      
+      Sky logger inputs details identical to the logbook that all pilots are
+      familiar with.
+    </p>
+    <p>
+      In addition, Sky Logger provides aggregate totals, which
+      simplify filling out FAA and insurance applications, which could take a pilot
+      significant time to compile with regulations to obtain their pilot license!
+    </p>
 
+  </div>
+)
 export default About;
