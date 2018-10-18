13322381e9de62eae4628c73540e5153bbc479cc
Cody <35708351+Cgranger29@users.noreply.github.com>
2018-10-18 17:00:11 -0400

    finishing sidebar update

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -10,9 +10,13 @@ const SideBar = props => {
     }
     return (
     <nav className = {sidebarClasses}>
+    <h1>------Flight Stats------</h1> 
         <ul>
-            <li>Total Hours Flown: {props.totalHours}  </li>
-            <li> Hours Year-To-Date: {props.hoursYTD} </li>
+            <li className='sidebarList'>Total Hours Flown: <h3 className='numSyle'>{props.totalHours}</h3>  </li>
+            <li className='sidebarList'> Hours Remaining Until License: <h3 className='numSyle'>{props.hoursRemaining}</h3> </li>
+            <li className='sidebarList'> Last Flight Remark: <h3 className='numSyle'>{props.lastRemark}</h3></li>
+            <li className='sidebarList'> Last Takeoff Location:<h3 className='numSyle'>{props.lastTakeoffLocation}</h3></li>
+            <li className='sidebarList'> Last Landing Location: <h3 className='numSyle'>{props.lastLandLocation}</h3></li>
         </ul>
     </nav>
     )
